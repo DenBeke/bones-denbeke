1bbce196cc0e7a639c01bf078a6d849fcae91126
MathiasB <bekemathias@gmail.com>
2014-05-18 15:02:30 +0200

    Added local jQuery

--- header.php
+++ header.php
@@ -40,6 +40,10 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/pure/buttons.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/pure/forms.css" />
 
+		<!-- Include jQuery -->
+		<script src="<?php echo get_template_directory_uri(); ?>/script/jquery.min.js" type="text/javascript"></script>		
+
+
 
 		<?php // wordpress head functions ?>
 		<?php wp_head(); ?>
